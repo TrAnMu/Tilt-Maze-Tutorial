77d7f8f2d221d793a13a154121a9ef9ee475cb3f
TrAnMu <Travis@wireless-10-105-6-217.umd.edu>
2017-02-08 02:01:07 -0500

    Barriers incoming

--- Tilt Maze/Assets/Scripts/Tilt.cs
+++ Tilt Maze/Assets/Scripts/Tilt.cs
@@ -15,19 +15,19 @@ public class Tilt : MonoBehaviour {
 	void Update () {
 		currentRot = GetComponent<Transform> ().eulerAngles;
 
-		if ((Input.GetAxis ("Horizontal") > 0.2) && (currentRot.z >= 351 || currentRot.z <=9)) {
+		if ((Input.GetAxis ("Horizontal") > 0.2) && (currentRot.z >= 349 || currentRot.z <= 11)) {
 			transform.Rotate (0, 0, -1);
 		}
 
-		if ((Input.GetAxis ("Horizontal") < -0.2) && (currentRot.z <= 8 || currentRot.z >= 350)) {
+		if ((Input.GetAxis ("Horizontal") < -0.2) && (currentRot.z <= 10 || currentRot.z >= 348)) {
 			transform.Rotate (0, 0, 1);
 		}  
 
-		if ((Input.GetAxis ("Vertical") > 0.2) && (currentRot.x <= 8 || currentRot.x >= 350)) {
+		if ((Input.GetAxis ("Vertical") > 0.2) && (currentRot.x <= 10 || currentRot.x >= 348)) {
 			transform.Rotate ( 1, 0, 0);
 		}
 
-		if ((Input.GetAxis ("Vertical") < -0.2) && (currentRot.x >= 351 || currentRot.x <=9)) {
+		if ((Input.GetAxis ("Vertical") < -0.2) && (currentRot.x >= 349 || currentRot.x <= 11)) {
 			transform.Rotate (-1, 0, 0); 
 		}
 	}
